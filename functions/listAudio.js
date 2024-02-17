caaebcc460d1f65350fe83d747a9df62a028fa6d
Fares <65826627+ScalarZ@users.noreply.github.com>
2024-02-17 14:40:16 +0100

    Update listAudio.js

--- functions/listAudio.js
+++ functions/listAudio.js
@@ -1,42 +1,42 @@
 const list = [
   {
     name: "Chill Gaming",
-    url: "https://www.fesliyanstudios.com/musicfiles/2019-06-07_-_Chill_Gaming_-_David_Fesliyan.mp3",
+    url: "https://github.com/Account04/rainsounds/raw/main/rain-in-forest-birds-nature-111405.mp3",
     favorite: false,
   },
   {
     name: "I Got This",
-    url: "https://www.fesliyanstudios.com/musicfiles/2019-06-12_-_I_Got_This_-_www.fesliyanstudios.com_-_David_Renda.mp3",
+    url: "https://github.com/Account04/rainsounds/raw/main/rain-in-forest-birds-nature-111405.mp3",
     favorite: false,
   },
   {
     name: "Tokyo LoFi",
-    url: "https://www.fesliyanstudios.com/musicfiles/2021-03-18_-_Tokyo_Lo-Fi_-_www.FesliyanStudios.com_Steve_Oxen/2021-03-18_-_Tokyo_Lo-Fi_-_www.FesliyanStudios.com_Steve_Oxen.mp3",
+    url: "https://github.com/Account04/rainsounds/raw/main/rain-in-forest-birds-nature-111405.mp3",
     favorite: false,
   },
   {
     name: "Homework",
-    url: "https://www.fesliyanstudios.com/musicfiles/2019-06-12_-_Homework_-_David_Fesliyan.mp3",
+    url: "https://github.com/Account04/rainsounds/raw/main/rain-in-forest-birds-nature-111405.mp3",
     favorite: false,
   },
   {
     name: "Steady Enjoyment",
-    url: "https://www.fesliyanstudios.com/musicfiles/2021-01-12_-_Steady_Enjoyment_-_www.FesliyanStudios.com_David_Renda/2021-01-12_-_Steady_Enjoyment_-_www.FesliyanStudios.com_David_Renda.mp3",
+    url: "https://github.com/Account04/rainsounds/raw/main/rain-in-forest-birds-nature-111405.mp3",
     favorite: false,
   },
   {
     name: "Time Alone",
-    url: "https://www.fesliyanstudios.com/musicfiles/2020-11-17_-_Time_Alone_-_www.FesliyanStudios.com_David_Renda/2020-11-17_-_Time_Alone_-_www.FesliyanStudios.com_David_Renda.mp3",
+    url: "https://github.com/Account04/rainsounds/raw/main/rain-in-forest-birds-nature-111405.mp3",
     favorite: false,
   },
   {
     name: "Mellow Thoughts",
-    url: "https://www.fesliyanstudios.com/musicfiles/2020-09-14_-_Mellow_Thoughts_-_www.FesliyanStudios.com_David_Renda/2020-09-14_-_Mellow_Thoughts_-_www.FesliyanStudios.com_David_Renda.mp3",
+    url: "https://github.com/Account04/rainsounds/raw/main/rain-in-forest-birds-nature-111405.mp3",
     favorite: false,
   },
   {
     name: "Looking Up",
-    url: "https://www.fesliyanstudios.com/musicfiles/2020-09-14_-_Looking_Up_-_www.FesliyanStudios.com_David_Renda/2020-09-14_-_Looking_Up_-_www.FesliyanStudios.com_David_Renda.mp3",
+    url: "https://github.com/Account04/rainsounds/raw/main/rain-in-forest-birds-nature-111405.mp3",
     favorite: false,
   },
 ];
